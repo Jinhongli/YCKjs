a0c875c569a6d678928268c8d322a01f3367ad8e
靳宏立(Eleven Jin) <jinhongli@qiyi.com>
2017-10-23 13:56:55 +0800

    modify style

--- app.js
+++ app.js
@@ -3,40 +3,35 @@ var fs = require('fs');
 var handlebars = require('handlebars');
 var chalk = require('chalk');
 
-fs.readFile('./template.hbs', 'utf8', function(err, template){
-    if (err) throw err;
-    var compile = handlebars.compile(template);
-    fs.readdir('./src', function(err, files) {
+fs.mkdir('./dist/', function(){
+    fs.readFile('./template.hbs', 'utf8', function(err, template){
         if (err) throw err;
-        var fileExtension = '';
-        var fileName = '';
-        var completeLog = '';
-        console.log('\n ' + chalk.yellow(files.length) + ' files found.');
-        console.log('-----------------------------');
-        for (var i=0; i<files.length; i++) {
-            var count = 0;
-            fileName = files[i].substr(0, files[i].lastIndexOf('.'));
-            fileExtension = files[i].substr(files[i].lastIndexOf('.')+1);
-            console.log(' ' + chalk.blue(i) + ': ' + files[i]);
-            fs.readFile('./src/' + files[i], 'utf8', function(err, md){
-                if (err) throw err;
-                var data = {
-                    title: 'Scope',
-                    md: marked(md)
-                }
-                var html = compile(data);
-                fs.writeFile('./dist/' + fileName + '.html', html, 'utf8', function(err) {
-                    if (err) throw err;
-                    count++;
-                    if (count === files.length) {
-                        completeLog += '. complete!';
-                        console.log(chalk.green(completeLog))
-                    } else {
-                        completeLog += '.';
-                    }
-                });
-            });
-        }
-    })
-
-});
+        var compile = handlebars.compile(template);
+        fs.readdir('./src', function(err, files) {
+            if (err) throw err;
+            console.log('\n ' + chalk.yellow(files.length) + ' files found.');
+            console.log('-----------------------------');
+            for (var i=0; i<files.length; i++) {
+                (function IIFE(i){
+                    // fileName = files[i].substr(0, files[i].lastIndexOf('.'));
+                    // fileExtension = files[i].substr(files[i].lastIndexOf('.')+1);
+                    console.log(' ' + chalk.blue(i) + ': ' + files[i]);
+                    fs.readFile('./src/' + files[i], 'utf8', function(err, md){
+                        if (err) throw err;
+                        var data = {
+                            title: 'Scope',
+                            md: marked(md)
+                        }
+                        var html = compile(data);
+                        fs.writeFile('./dist/' + files[i].substr(0, files[i].lastIndexOf('.')) + '.html', html, 'utf8', function(err) {
+                            console.log(i)
+                            if (err) {
+                                throw err;
+                            }
+                        });
+                    });
+                })(i);
+            }
+        });
+    });
+})
